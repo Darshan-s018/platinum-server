c38db7111b3cbcd93b91bc72dba1487e103f0f43
Darshan S <darshu8988@gmail.com>
2025-05-05 10:26:41 +0530

    initialize project structure with essential configuration files and setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+
+const allroutes = new Hono();
+
+allroutes.get('', (context) => {
+  return context.json({
+    message: 'Hello World!',
+});
